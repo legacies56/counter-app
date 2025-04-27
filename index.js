c30ff5cce39a584d68d10a44a2f7603a464d09b3
legacies56 <mohamedanwer3061@gmail.com>
2025-04-27 12:01:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
-// document.getElementById("count-el").innerText = 5
 
+// let count = 5
+// count = count + 1
+// console.log(count)
+
+// intialize the count as 0
+// listen for clicks on the increment button
+// increment the count variable when the button is clicked
 // change the count-el in the HTML to reflect the new count
 
-// camelCase
-let countEl = document.getElementById("count-el")
-
-console.log(countEl)
-
-let count = 0
-
 function increment() {
-    count = count + 1
-    countEl.innerText = count
+    console.log("The button was clicked")
 }
 
-function save() {
-    count = count + 5
-    countEl.innerText = count
-}
+
