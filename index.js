053add5f71b960355521d0cc1f6af4d4f8b760ea
legacies56 <mohamedanwer3061@gmail.com>
2025-04-27 12:25:07 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// document.getElementById("count").innerText = 5
 
 // let count = 5
 // count = count + 1
@@ -5,13 +6,14 @@
 
 // intialize the count as 0
 // listen for clicks on the increment button
-// increment the count variable when the button is clicked
+// increment the count variable when the button is clicked (log it out)
 // change the count-el in the HTML to reflect the new count
 
-function increment() {
-    console.log("The button was clicked")
-}
+let count = "eat a bybi"
 
-increment()
+function increment() {
+    count = count + ", eat more bybius"
+    console.log(count)
+}
 
 
